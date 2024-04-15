2a990186300792da28b4be8431d875233f1a48a5
spiralcodebase <sapkotapawan011@gmail.com>
2024-04-15 09:00:46 +0545

    completed

--- Array&String/605_Flower.py
+++ Array&String/605_Flower.py
@@ -3,7 +3,15 @@ from typing import List
 
 
 def canPlaceFlowers(flowerbed: List[int], n: int) -> bool:
-    pass 
+    if n == 0:
+            return True
+    for i in range(len(flowerbed)):
+        if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+            flowerbed[i] = 1
+            n -= 1
+            if n == 0:
+                return True
+    return False 
 
 
 
